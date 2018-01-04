0668d3369efc8c19a4be9258c237cf56c60cd4f3
Rafael Cardoso <raf07ray@gmail.com>
2018-01-04 14:02:13 -0300

    working like a charm

--- corr.R
+++ corr.R
@@ -9,4 +9,29 @@ corr <- function(directory, threshold = 0){
 
 	## Return a numeric vector of correlations
 	## NOTE: Do not round the result!
+  
+  ## setting parent directory
+  setwd("~/Documentos/R_studio_WD/Quiz02_data")
+  
+  ## initialize an empty data frame
+  cr_data <- numeric()
+  index <- 1
+  all_files <- list.files(paste(directory,'/', sep = ""))
+  for (file_id in all_files){
+      ## read file dataframe
+      data <- read.csv(paste(directory,'/',file_id, sep = ""))
+      
+      ## identify what is the avaliable  sulfate data
+      sulf_data <- data["sulfate"][!(is.na(data["sulfate"]) | is.na(data["nitrate"]))]
+      
+      if (length(sulf_data) > threshold){
+        ## identify what is the avaliable nitrate data
+        nitr_data <- data["nitrate"][!(is.na(data["sulfate"]) | is.na(data["nitrate"]))]
+        
+        ## calculate and store correlation
+        cr_data[index] <- cor(sulf_data,nitr_data)
+        index <- index+1
+      }
+  }
+  cr_data
 }
